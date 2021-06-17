56849d2eba50c5f90521bba842e711fa05354bb2
AdpRP <83350186+AdpRP@users.noreply.github.com>
2021-06-17 23:40:47 +0100

    Create Ldados.py

---
+++ Ldados.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 19:56:07 2021
+
+@author: ap
+"""
+def al():
+    
+    lista=[]
+    colunas0=2
+    alunosinseridos=4
+    c = 0
+    d=0
+    n=0
+    no=0
+    si=0
+    for i in range(colunas0): #colunas 
+        nome=[]
+        numero=[]
+        nota=[]
+    
+    for j in range(alunosinseridos): #insercao dos valores nas listas nome numero e nota
+        c+=1
+        numero.append(c)
+        nome.append(input('Digite seu nome: '))
+        nota.append(input('Digite a nota: '))
+    
+    lista.append(numero)
+    lista.append(nome)
+    lista.append(nota)
+    
+    print('\n lista completa')
+    print('\n',lista,'\n')
+    
+    for i in range(2):
+        while d!=alunosinseridos:
+            print('Sigu',numero[d],'Nome',nome[d],'Nota: ',nota[d])
+            print("""""")
+            d=d+1
+        break
+        #print('Nome',nome[d],'Nota: ',nota[d])
+    for i in range(2):
+        while n!=alunosinseridos:
+            print('\n Nome',nome[n])
+            print("""""")
+            n=n+1
+    for i in range(2):
+        while no!=alunosinseridos:
+            print('\n Nota: ',nota[no])
+            print("""""")
+            no=no+1
+    for i in range(2):
+        while si!=alunosinseridos:
+            print('\n Numero sigu ',numero[si])
+            print("""""")
+            si=si+1
